2e2b22d5aa9f1a402654766db4851398bf7fe833
Rubén Vidal <ruben.vidal.castellet@gmail.com>
2011-04-13 16:55:11 +0200

    Aplicación inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vet::Application.config.secret_token = 'a465cbb468c7b5b2006c643d514927ae699f522fecb2332d3577ca02ad9bf9aada8aeaddccd2d7412498b9ab74f39f2a177bf06f7ffa9665f214d2f8aee206b0'
